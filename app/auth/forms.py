d2865964ae0163f2099624db6eac0108ed055628
Aleksey Ponomarev <exeyp@yandex.ru>
2024-11-19 10:32:11 +0800

    Add styles and CSRFProtect

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired(), Length(min=4, max=20)])
-    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=4, max=128)])
-    submit = SubmitField('Войти')
+    username = StringField('Username')
+    password = PasswordField('Password')
+    submit = SubmitField('Log in')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,24 +1,25 @@
 from flask import render_template, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import current_user, login_user, logout_user, login_required
 from . import auth_bp
 from .forms import LoginForm
 from ..models import User
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard.index'))
+    
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data, deleted=False).first()
         if user and user.check_password(form.password.data):
             login_user(user)
-            flash('Вы успешно вошли!', 'success')
             return redirect(url_for('dashboard.index'))
-        flash('Неверное имя пользователя или пароль', 'danger')
+        flash('Invalid username or password', 'danger')
     return render_template('auth/login.html', form=form)
 
 @auth_bp.route('/logout')
 @login_required
 def logout():
     logout_user()
-    flash('Вы вышли из системы.', 'info')
     return redirect(url_for('auth.login'))
